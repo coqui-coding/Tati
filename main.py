62870b8cdae21fbbcc9c18425901d18d7fd88d0e
DVT <DVT12@protonmail.com>
2021-08-11 13:31:48 -0400

    Remove version number until major release

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # Weather app that detects changes from NHC page and sends email to subscribers
-__version__ = '21.08.11'
 
 import requests
 from bs4 import BeautifulSoup
@@ -10,8 +9,6 @@ logging.basicConfig(level=logging.DEBUG)
 nhc_site = 'https://www.nhc.noaa.gov/'
 
 if __name__ == '__main__':
-    logging.info(f'Running version: {__version__}')
-
     source = requests.get(nhc_site).text
     soup = BeautifulSoup(source, 'lxml')
     tables = soup.find_all('table')
